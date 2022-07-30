62eb0dd5bcc11af410b09f645ac5e820aaf92636
shambrgler <s.wayne.wells@gmail.com>
2022-07-29 18:03:09 -0600

    Initial commit

---
+++ app/src/main/java/com/wgu/scheduleapp/UI/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wgu.scheduleapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wgu.scheduleapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
