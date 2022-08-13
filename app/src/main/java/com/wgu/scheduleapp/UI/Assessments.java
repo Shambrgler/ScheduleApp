14f9374ac6bee8b7df768a8776b4570bca58f873
shambrgler <s.wayne.wells@gmail.com>
2022-08-13 14:09:45 -0600

    New Screens Template

--- app/src/main/java/com/wgu/scheduleapp/UI/Assessments.java
+++ app/src/main/java/com/wgu/scheduleapp/UI/Assessments.java
@@ -2,7 +2,9 @@ package com.wgu.scheduleapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.wgu.scheduleapp.R;
 
@@ -12,5 +14,10 @@ public class Assessments extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assesments);
+    }
+
+    public void openNewAssessment(View view) {
+        Intent intent = new Intent(Assessments.this, NewAssessment.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/wgu/scheduleapp/UI/Courses.java
+++ app/src/main/java/com/wgu/scheduleapp/UI/Courses.java
@@ -2,7 +2,9 @@ package com.wgu.scheduleapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.wgu.scheduleapp.R;
 
@@ -12,5 +14,10 @@ public class Courses extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+    }
+
+    public void openNewCourse(View view) {
+        Intent intent = new Intent(Courses.this, NewCourse.class);
+        startActivity(intent);
     }
 }
